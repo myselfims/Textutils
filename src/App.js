efc80f44bd5ece1e1f43473c732e716eb9660909
myselfims <shaikhimran7585@gmail.com>
2023-05-11 10:46:22 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import Navbar from './Components/Navbar';
 import './App.css';
+import Form from './Components/Form';
+import { useState } from 'react';
+import Alert from './Components/Alert';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
+  const [mode,setMode] = useState('light')
+  
+
+  const toggleMode = () => {
+    console.log('working')
+    if (mode === 'dark'){
+      setMode('light')
+      document.body.style.backgroundColor = 'white';
+      document.body.style.color = 'black';
+    }else{
+      document.body.style.backgroundColor = 'black';
+      document.body.style.color = 'white';
+      setMode('dark')
+    }
+  }
+  const [alertobj,setAlert] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Router> */}
+   <Navbar toggler={toggleMode} mode={mode} />
+   <Alert alert={alertobj}/>
+   <div  className="container">
+    <Form />
+    {/* <Routes> */}
+      {/* <Route exact path="/form" Component={Form}/>      */}
+    {/* </Routes> */}
+
+   </div>
+   {/* </Router> */}
+    </>
   );
 }
 
 export default App;
+
+
---
+++ src/Components/Form.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+
+
+export default function Form() {
+    const [text,setText] = useState('Hello World')
+    const [textMode, setTextMode] = useState('Encrypt')
+
+    const changeHandler = (event)=>{
+        setText(event.target.value)
+        if (event.target.value.at(-1) in encrypt_char){
+            setTextMode('Encrypt')
+        }else{
+            setTextMode('Decrypt')
+        }
+    }
+
+    const toUpperCase = ()=>{
+        setText(text.toUpperCase())
+    }
+    const toLowerCase = ()=>{
+        setText(text.toLowerCase())
+    }
+
+    const encrypt_char = {
+        a : '@',
+        b : '#',
+        c : '!',
+        d : '$',
+        e : '^',
+        f : '&',
+        g : '*',
+        h : '(',
+        i : ')',
+        j : '-',
+        k : '+',
+        l : '=',
+        m : ':',
+        n : ';',
+        o : '"',
+        p : "'",
+        q : '[',
+        r : ']',
+        s : '{',
+        t : '}',
+        u : '|',
+        v : '?',
+        w : '~',
+        x : '`',
+        y : '<',
+        z : '>'   
+    }
+
+    const encrypt = ()=>{
+        let newText = '';
+        if (textMode === 'Encrypt'){
+            for (let char of text){ 
+                if (char !== ' '){
+                    newText += encrypt_char[char.toLowerCase()]
+                }else{
+                    newText += ',';
+    
+                }
+            }
+            setTextMode('Decrypt')
+
+        }else{
+            for (let char of text){
+                if (char !== ','){
+                    newText += Object.keys(encrypt_char).find(key => encrypt_char[key] === char)
+                }else{
+                    newText += ' ';
+
+                }
+            }
+            setTextMode('Encrypt')
+        }
+        setText(newText)
+
+    }
+    const decrypt = ()=>{
+        let newText = '';
+        for (let char of text){
+            if (char !== ','){
+                newText += Object.keys(encrypt_char).find(key => encrypt_char[key] === char)
+            }else{
+                newText += ' ';
+
+            }
+        }
+        setText(newText)
+
+    }
+
+  return (
+    <>
+    <div>
+        <div className="mb-3">
+        <h1>Enter text here</h1>
+        <textarea value={text} className="form-control" id="exampleFormControlTextarea1" onChange={changeHandler} rows="8"></textarea>
+        <button onClick={toUpperCase} className="btn btn-primary my-4 mx-3">UPPER CASE</button>
+        <button onClick={toLowerCase} className="btn btn-primary my-4 mx-3">UPPER CASE</button>
+        <button onClick={encrypt} className="btn btn-primary my-4 mx-3">{textMode}</button>
+        </div>
+    </div>
+    <div className="container">
+        <h1>Your text summary.</h1>
+        <p>Word : {text.split(' ').length}</p>
+        <p>Characters : {text.length}</p>
+        <p>Read time : { 0.008 * text.split(' ').length}</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  )
+  
+}
+
+
+Form.prototype = {text : String.prototype}
