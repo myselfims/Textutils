3b2c1203397ca4b642e09b47585c4f2bba4df41d
myselfims <shaikhimran7585@gmail.com>
2023-05-18 19:53:38 +0530

    translation added

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,13 @@
 import React, { useState } from 'react'
+import { getKruti, unicode } from '.';
 
 
 export default function Form(props) {
-    const [text,setText] = useState('Hello World')
+    const [text,setText] = useState('')
+    const [krutibtn,setKrutiBtn] = useState(true)
     const [textMode, setTextMode] = useState('Encrypt')
+    const [tranlatebtn, setTransBtn] = useState(false)
+    const [encryptionBtn, setencryptionBtn] = useState(false)
 
     const changeHandler = (event)=>{
         setText(event.target.value)
@@ -12,6 +16,26 @@ export default function Form(props) {
         }else{
             setTextMode('Decrypt')
         }
+        if (event.target.value.at(0) in unicode || text.length === 0){
+            setTransBtn(true)
+            setKrutiBtn(false)
+            setencryptionBtn(true)
+        }else{
+            setKrutiBtn(true)
+            setTransBtn(false)
+            setencryptionBtn(false)
+        }
+    }
+
+    const translateToHindi = async ()=>{
+        let oldtext = text
+        setText('Translating...')
+        let url = `https://api.mymemory.translated.net/get?q=${oldtext}&langpair=en|hi`
+        let response = await fetch(url);
+        let translated = await response.json()
+        setText(translated.responseData['translatedText'])
+        setKrutiBtn(false)
+        setTransBtn(true)
     }
 
     const toUpperCase = ()=>{
@@ -75,8 +99,13 @@ export default function Form(props) {
             setTextMode('Encrypt')
         }
         setText(newText)
-
     }
+
+    const copyKrutidev = ()=>{
+        window.navigator.clipboard.writeText(getKruti(text))
+        alert('Copied...')
+    }
+
 
     const [copyBtnText,setCopyText] = useState('Copy')
     const copyText = ()=>{
@@ -87,16 +116,37 @@ export default function Form(props) {
         }, 500);
     }
 
+    const clearText = () => {
+        setText('')
+    }
+
   return (
     <>
     <div>
         <div className="mb-3">
-        <h1>Enter text here</h1>
-        <textarea value={text} className={`form-control bg-${props.mode} text-${props.mode==='dark'?'light':'dark'}`}id="exampleFormControlTextarea1" onChange={changeHandler} rows="8"></textarea>
-        <button disabled={text.length===0} onClick={toUpperCase} className="btn btn-primary my-4 mx-3">UPPER CASE</button>
-        <button disabled={text.length===0} onClick={toLowerCase} className="btn btn-primary my-4 mx-3">UPPER CASE</button>
+        <div className="row my-4 d-flex align-items-center">
+            <div className="col md-6">
+            <h1>Enter text here</h1>
+                
+            </div>
+            <div className="col md-6">
+
+            {text.length !== 0 && <button onClick={clearText} type="button" className="btn btn-danger" aria-label="Close">Clear</button>}
+            </div>
+
+        </div>
+        <textarea placeholder='Type here' id='transliterateTextarea' value={text} className={`form-control bg-${props.mode} text-${props.mode==='dark'?'light':'dark'}`} onChange={changeHandler} rows="8"></textarea>
+        <button disabled={text.length===0} onClick={toUpperCase} className="btn btn-primary my-4 mx-3"><i className="fa-solid fa-arrow-up"></i> UPPER CASE</button>
+
+        <button disabled={text.length===0} onClick={toLowerCase} className="btn btn-primary my-4 mx-3"><i className="fa-solid fa-arrow-down"></i> Lower CASE</button>
+
+        <button disabled={krutibtn || text.length===0} onClick={copyKrutidev} className="btn btn-primary my-4 mx-3"><i className="fa-regular fa-copy"></i> Copy Krutidev</button>
+
+        <button disabled={tranlatebtn || text.length===0} onClick={translateToHindi} className="btn btn-primary my-4 mx-3"><i className="fa-solid fa-language"></i> Translate to hindi</button>
+
         <button disabled={text.length===0} onClick={copyText} className="btn btn-primary my-4 mx-3"><i className="fa-regular fa-copy"></i> {copyBtnText}</button>
-        <button disabled={text.length===0} onClick={encrypt} className="btn btn-primary my-4 mx-3">{textMode}</button>
+        <button disabled={encryptionBtn && text.length!==0 || text.length===0} onClick={encrypt} className="btn btn-primary my-4 mx-3">{textMode}</button>
+
         </div>
     </div>
     <div className="container">
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <script src="/src/Components/jsapi.js"></script>
+  <script src="/src/Components/Translator.js"></script>
     <App />
   </React.StrictMode>
 );
